0ca33716005f39c861d4b08dfc92ebc43b2e705d
George Fotopoulos <84932056+xorz57@users.noreply.github.com>
2024-10-29 12:03:39 +0200

    Update FNV.hpp

--- FNV.hpp
+++ FNV.hpp
@@ -2,66 +2,68 @@
 
 #include <cstdint>
 
-namespace fnv0 {
-    constexpr std::uint32_t hash32(const char* str) noexcept {
-        std::uint32_t prime = 0x1000193;
-        std::uint32_t hash = 0;
-        for (; *str != '\0'; ++str) {
-            hash = (hash * prime) ^ static_cast<std::uint32_t>(static_cast<uint8_t>(*str));
+namespace fnv {
+    namespace v0 {
+        constexpr std::uint32_t hash32(const char* str) noexcept {
+            std::uint32_t prime = 0x1000193;
+            std::uint32_t hash = 0;
+            for (; *str != '\0'; ++str) {
+                hash = (hash * prime) ^ static_cast<std::uint32_t>(static_cast<uint8_t>(*str));
+            }
+            return hash;
         }
-        return hash;
+
+        constexpr std::uint64_t hash64(const char* str) noexcept {
+            std::uint64_t prime = 0x100000001b3;
+            std::uint64_t hash = 0;
+            for (; *str != '\0'; ++str) {
+                hash = (hash * prime) ^ static_cast<std::uint64_t>(static_cast<uint8_t>(*str));
+            }
+            return hash;
+        }
     }
 
-    constexpr std::uint64_t hash64(const char* str) noexcept {
-        std::uint64_t prime = 0x100000001b3;
-        std::uint64_t hash = 0;
-        for (; *str != '\0'; ++str) {
-            hash = (hash * prime) ^ static_cast<std::uint64_t>(static_cast<uint8_t>(*str));
+    namespace v1 {
+        constexpr std::uint32_t hash32(const char* str) noexcept {
+            std::uint32_t prime = 0x1000193;
+            std::uint32_t offset_basis = 0x811c9dc5;
+            std::uint32_t hash = offset_basis;
+            for (; *str != '\0'; ++str) {
+                hash = (hash * prime) ^ static_cast<std::uint32_t>(static_cast<uint8_t>(*str));
+            }
+            return hash;
         }
-        return hash;
-    }
-}
 
-namespace fnv1 {
-    constexpr std::uint32_t hash32(const char* str) noexcept {
-        std::uint32_t prime = 0x1000193;
-        std::uint32_t offset_basis = 0x811c9dc5;
-        std::uint32_t hash = offset_basis;
-        for (; *str != '\0'; ++str) {
-            hash = (hash * prime) ^ static_cast<std::uint32_t>(static_cast<uint8_t>(*str));
+        constexpr std::uint64_t hash64(const char* str) noexcept {
+            std::uint64_t prime = 0x100000001b3;
+            std::uint64_t offset_basis = 0xcbf29ce484222325;
+            std::uint64_t hash = offset_basis;
+            for (; *str != '\0'; ++str) {
+                hash = (hash * prime) ^ static_cast<std::uint64_t>(static_cast<uint8_t>(*str));
+            }
+            return hash;
         }
-        return hash;
-    }
-
-    constexpr std::uint64_t hash64(const char* str) noexcept {
-        std::uint64_t prime = 0x100000001b3;
-        std::uint64_t offset_basis = 0xcbf29ce484222325;
-        std::uint64_t hash = offset_basis;
-        for (; *str != '\0'; ++str) {
-            hash = (hash * prime) ^ static_cast<std::uint64_t>(static_cast<uint8_t>(*str));
-        }
-        return hash;
-    }
-}
-
-namespace fnv1a {
-    constexpr std::uint32_t hash32(const char* str) noexcept {
-        std::uint32_t prime = 0x1000193;
-        std::uint32_t offset_basis = 0x811c9dc5;
-        std::uint32_t hash = offset_basis;
-        for (; *str != '\0'; ++str) {
-            hash = (hash ^ static_cast<std::uint32_t>(static_cast<uint8_t>(*str))) * prime;
-        }
-        return hash;
     }
 
-    constexpr std::uint64_t hash64(const char* str) noexcept {
-        std::uint64_t prime = 0x100000001b3;
-        std::uint64_t offset_basis = 0xcbf29ce484222325;
-        std::uint64_t hash = offset_basis;
-        for (; *str != '\0'; ++str) {
-            hash = (hash ^ static_cast<std::uint64_t>(static_cast<uint8_t>(*str))) * prime;
+    namespace v1a {
+        constexpr std::uint32_t hash32(const char* str) noexcept {
+            std::uint32_t prime = 0x1000193;
+            std::uint32_t offset_basis = 0x811c9dc5;
+            std::uint32_t hash = offset_basis;
+            for (; *str != '\0'; ++str) {
+                hash = (hash ^ static_cast<std::uint32_t>(static_cast<uint8_t>(*str))) * prime;
+            }
+            return hash;
         }
-        return hash;
+
+        constexpr std::uint64_t hash64(const char* str) noexcept {
+            std::uint64_t prime = 0x100000001b3;
+            std::uint64_t offset_basis = 0xcbf29ce484222325;
+            std::uint64_t hash = offset_basis;
+            for (; *str != '\0'; ++str) {
+                hash = (hash ^ static_cast<std::uint64_t>(static_cast<uint8_t>(*str))) * prime;
+            }
+            return hash;
+        }
     }
 }
