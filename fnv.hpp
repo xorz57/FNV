ba1fdd2d2daa034d8bb043351a0c3585728f8efc
George Fotopoulos <84932056+xorz57@users.noreply.github.com>
2024-10-29 12:12:34 +0200

    Update fnv.hpp

--- fnv.hpp
+++ fnv.hpp
@@ -5,7 +5,7 @@
 namespace fnv {
     namespace fnv0 {
         constexpr std::uint32_t hash32(const char* str) noexcept {
-            std::uint32_t prime = 0x1000193;
+            constexpr std::uint32_t prime = 0x1000193;
             std::uint32_t hash = 0;
             for (; *str != '\0'; ++str) {
                 hash = (hash * prime) ^ static_cast<std::uint32_t>(static_cast<uint8_t>(*str));
@@ -14,7 +14,7 @@ namespace fnv {
         }
 
         constexpr std::uint64_t hash64(const char* str) noexcept {
-            std::uint64_t prime = 0x100000001b3;
+            constexpr std::uint64_t prime = 0x100000001b3;
             std::uint64_t hash = 0;
             for (; *str != '\0'; ++str) {
                 hash = (hash * prime) ^ static_cast<std::uint64_t>(static_cast<uint8_t>(*str));
@@ -25,8 +25,8 @@ namespace fnv {
 
     namespace fnv1 {
         constexpr std::uint32_t hash32(const char* str) noexcept {
-            std::uint32_t prime = 0x1000193;
-            std::uint32_t offset_basis = 0x811c9dc5;
+            constexpr std::uint32_t prime = 0x1000193;
+            constexpr std::uint32_t offset_basis = 0x811c9dc5;
             std::uint32_t hash = offset_basis;
             for (; *str != '\0'; ++str) {
                 hash = (hash * prime) ^ static_cast<std::uint32_t>(static_cast<uint8_t>(*str));
@@ -35,8 +35,8 @@ namespace fnv {
         }
 
         constexpr std::uint64_t hash64(const char* str) noexcept {
-            std::uint64_t prime = 0x100000001b3;
-            std::uint64_t offset_basis = 0xcbf29ce484222325;
+            constexpr std::uint64_t prime = 0x100000001b3;
+            constexpr std::uint64_t offset_basis = 0xcbf29ce484222325;
             std::uint64_t hash = offset_basis;
             for (; *str != '\0'; ++str) {
                 hash = (hash * prime) ^ static_cast<std::uint64_t>(static_cast<uint8_t>(*str));
@@ -47,8 +47,8 @@ namespace fnv {
 
     namespace fnv1a {
         constexpr std::uint32_t hash32(const char* str) noexcept {
-            std::uint32_t prime = 0x1000193;
-            std::uint32_t offset_basis = 0x811c9dc5;
+            constexpr std::uint32_t prime = 0x1000193;
+            constexpr std::uint32_t offset_basis = 0x811c9dc5;
             std::uint32_t hash = offset_basis;
             for (; *str != '\0'; ++str) {
                 hash = (hash ^ static_cast<std::uint32_t>(static_cast<uint8_t>(*str))) * prime;
@@ -57,8 +57,8 @@ namespace fnv {
         }
 
         constexpr std::uint64_t hash64(const char* str) noexcept {
-            std::uint64_t prime = 0x100000001b3;
-            std::uint64_t offset_basis = 0xcbf29ce484222325;
+            constexpr std::uint64_t prime = 0x100000001b3;
+            constexpr std::uint64_t offset_basis = 0xcbf29ce484222325;
             std::uint64_t hash = offset_basis;
             for (; *str != '\0'; ++str) {
                 hash = (hash ^ static_cast<std::uint64_t>(static_cast<uint8_t>(*str))) * prime;
